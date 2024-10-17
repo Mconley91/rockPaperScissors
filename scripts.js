ea79e069caa1796f3d419975e6bb050107cdbcf3
mconley91 <matthew.e.conley@gmail.com>
2024-10-17 19:36:48 -0400

    add turn limit add invalid entry catcher add instructions add case insensitivity to input

--- scripts.js
+++ scripts.js
@@ -1,25 +1,36 @@
-//Valid Player Entries
-let Rock = '';
-let Paper = '';
-let Scissors = '';
-//CPU Player logic:
-let randomNum = Math.floor(Math.random() * 3) + 1;
-let cpuChoice = randomNum == 1 ? 'Rock' : randomNum == 2 ? 'Paper' : randomNum == 3 ? 'Scissors' : 'ERROR';
-//Prompt:
-console.log('Enter "Rock", "Paper", or "Scissors" to begin');
-
-if(cpuChoice == 'Rock'){
-    Rock = "Computer picks Rock, it's a draw!";
-    Paper = "Computer picks Rock, You win!!";
-    Scissors = "Computer picks Rock, Computer wins!";
+for(let i = 0; i < 3; i++){
+    //prompt:
+    const play = prompt('Open the web console to play. Enter "Rock", "Paper", or "Scissors" to begin!').toLowerCase();
+    //CPU player logic:
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    let cpuChoice = randomNum == 1 ? 'Rock' : randomNum == 2 ? 'Paper' : randomNum == 3 ? 'Scissors' : 'ERROR';
+    //input handling
+    if(play != 'rock' && play != 'paper' && play != 'scissors'){
+        //alerts player to invalid entry & stalls the advancement of turns
+        alert('Invalid Entry');
+        i--;
+    }
+    else{
+        if (cpuChoice=='Rock'){
+            play == 'rock' ? console.log("Computer picks Rock, it's a draw!") : 
+            play == 'paper' ? console.log("Computer picks Rock, You win!!") :
+            play == 'scissors' ? console.log("Computer picks Rock, Computer wins!") : console.log('null');
+            console.log('You picked: ' + play);
+        }
+        else if (cpuChoice=='Paper'){
+            play == 'rock' ? console.log("Computer picks Paper, Computer wins!") : 
+            play == 'paper' ? console.log("Computer picks Paper, it's a draw!") :
+            play == 'scissors' ? console.log("Computer picks Paper, You win!!") : console.log('null');
+            console.log('You picked: ' + play);
+        }
+        else if (cpuChoice=='Scissors'){
+            play == 'rock' ? console.log("Computer picks Scissors, You win!!") : 
+            play == 'paper' ? console.log("Computer picks Scissors, Computer wins!") :
+            play == 'scissors' ? console.log("Computer picks Scissors, it's a draw!") : console.log('null');
+            console.log('You picked: ' + play);
+        }
+    };
+    // troubleshooting
+    // console.log('Random number was: ' + randomNum);
 }
-else if(cpuChoice == 'Paper'){
-    Rock = "Computer picks Paper, Computer wins!";
-    Paper = "Computer picks Paper, it's a draw!";
-    Scissors = "Computer picks Paper, You win!!";
-}
-else if(cpuChoice == 'Scissors'){
-    Rock = "Computer picks Scissors, You win!!";
-    Paper = "Computer picks Scissors, Computer wins!";
-    Scissors = "Computer picks Scissors, it's a draw!";
-}
+console.log('To play again, refresh this webpage');
