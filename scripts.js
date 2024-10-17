5e85a9820bd59ee1d6b18d5b00721098794ddcaf
mconley91 <matthew.e.conley@gmail.com>
2024-10-17 17:56:35 -0400

    add ternary operator & random variable

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@
 let Rock = '';
 let Paper = '';
 let Scissors = '';
-
+let randomNum = Math.floor(Math.random() * 3) + 1;
+let cpuChoice = randomNum == 1 ? 'Rock' : randomNum == 2 ? 'Paper' : randomNum == 3 ? Scissors : 'ERROR';
 console.log('Enter "Rock", "Paper", or "Scissors" to begin');
