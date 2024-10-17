6908bb2b7ec6e74a9e628d578373627dee2d8379
mconley91 <matthew.e.conley@gmail.com>
2024-10-17 17:50:29 -0400

    add console prompt and initial R.P.S variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-'use strict'
+// 'use strict'
+// let choice = prompt('Enter "Rock", "Paper", or "Scissors" to begin');
+let Rock = '';
+let Paper = '';
+let Scissors = '';
+
+console.log('Enter "Rock", "Paper", or "Scissors" to begin');
