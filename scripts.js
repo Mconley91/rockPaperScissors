7275431c2767c3e9cf08e008a31483d1a23ee19c
mconley91 <matthew.e.conley@gmail.com>
2024-10-17 18:05:37 -0400

    add branching if logic to determin winner

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
-// 'use strict'
-// let choice = prompt('Enter "Rock", "Paper", or "Scissors" to begin');
+//Valid Player Entries
 let Rock = '';
 let Paper = '';
 let Scissors = '';
+//CPU Player logic:
 let randomNum = Math.floor(Math.random() * 3) + 1;
-let cpuChoice = randomNum == 1 ? 'Rock' : randomNum == 2 ? 'Paper' : randomNum == 3 ? Scissors : 'ERROR';
+let cpuChoice = randomNum == 1 ? 'Rock' : randomNum == 2 ? 'Paper' : randomNum == 3 ? 'Scissors' : 'ERROR';
+//Prompt:
 console.log('Enter "Rock", "Paper", or "Scissors" to begin');
+
+if(cpuChoice == 'Rock'){
+    Rock = "Computer picks Rock, it's a draw!";
+    Paper = "Computer picks Rock, You win!!";
+    Scissors = "Computer picks Rock, Computer wins!";
+}
+else if(cpuChoice == 'Paper'){
+    Rock = "Computer picks Paper, Computer wins!";
+    Paper = "Computer picks Paper, it's a draw!";
+    Scissors = "Computer picks Paper, You win!!";
+}
+else if(cpuChoice == 'Scissors'){
+    Rock = "Computer picks Scissors, You win!!";
+    Paper = "Computer picks Scissors, Computer wins!";
+    Scissors = "Computer picks Scissors, it's a draw!";
+}
