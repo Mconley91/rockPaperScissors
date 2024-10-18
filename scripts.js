863a734204ede51d12ad1cd4eeaa6cef478bec32
Matt Conley <matthew.e.conley@gmail.com>
2024-10-18 01:02:02 -0400

    add cheat 'shotgun' for player to automatically win the game

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 //score tracker
 let playerScore = 0;
 let computerScore = 0;
+let cheating = false;
 //loop to handle turns
 for(let i = 0; i < 5; i++){
     //prompt:
@@ -9,10 +10,15 @@ for(let i = 0; i < 5; i++){
     let randomNum = Math.floor(Math.random() * 3) + 1;
     let cpuChoice = randomNum == 1 ? 'rock' : randomNum == 2 ? 'paper' : randomNum == 3 ? 'scissors' : 'ERROR';
     //input handling
-    if(play != 'rock' && play != 'paper' && play != 'scissors'){
+    if(play != 'rock' && play != 'paper' && play != 'scissors' && play != 'shotgun'){
         //alerts player to invalid entry & stalls the advancement of turns
         alert('Invalid Entry');
         i--;
+    }
+    else if(play == 'shotgun'){
+        console.log('CHEAT UNLOCKED: Player wins the game!');
+        cheating = true;
+        break;
     }
     else{
         if (cpuChoice=='rock'){
@@ -33,7 +39,9 @@ for(let i = 0; i < 5; i++){
     };
 };
 //final output
-console.log(`Final Score= Player: ${playerScore} Computer: ${computerScore}`);
-playerScore > computerScore ? console.log('You won the game!') : 
-playerScore == computerScore ? console.log("It's a tie!") : 
-console.log('Computer wins the game!');
+if (!cheating){
+    console.log(`Final Score= Player: ${playerScore} Computer: ${computerScore}`);
+    playerScore > computerScore ? console.log('You won the game!') : 
+    playerScore == computerScore ? console.log("It's a tie!") : 
+    console.log('Computer wins the game!');
+}
