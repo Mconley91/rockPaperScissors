5f4d6ce1c8ce1bb61c3c2ad26150f8db25541d1f
mconley91 <matthew.e.conley@gmail.com>
2024-10-17 20:08:31 -0400

    remove whitespace from code. fixed indentation in code.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
-    //score tracker
-    let playerScore = 0;
-    let computerScore = 0;
-
+//score tracker
+let playerScore = 0;
+let computerScore = 0;
+//loop to handle turns
 for(let i = 0; i < 3; i++){
     //prompt:
     const play = prompt('Open the web console to play. The game is best 2 out of 3. Enter "rock", "paper", or "scissors" to begin!').toLowerCase();
     //CPU player logic:
     let randomNum = Math.floor(Math.random() * 3) + 1;
     let cpuChoice = randomNum == 1 ? 'rock' : randomNum == 2 ? 'paper' : randomNum == 3 ? 'scissors' : 'ERROR';
-    
     //input handling
     if(play != 'rock' && play != 'paper' && play != 'scissors'){
         //alerts player to invalid entry & stalls the advancement of turns
@@ -20,19 +19,16 @@ for(let i = 0; i < 3; i++){
             play == 'rock' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) : 
             play == 'paper' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`),playerScore++) :
             play == 'scissors' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++) : console.log('massive error has occured');
-            
         }
         else if (cpuChoice=='paper'){
             play == 'rock' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++ ) : 
             play == 'paper' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) :
             play == 'scissors' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`), playerScore++) : console.log('massive error has occured');
-            
         }
         else if (cpuChoice=='scissors'){
             play == 'rock' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`),playerScore++) : 
             play == 'paper' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++) :
             play == 'scissors' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) : console.log('massive error has occured');
-            
         }
     };
 };
