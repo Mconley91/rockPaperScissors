64d66cbba71082b1d6f5881edf74d1f02f0c03aa
mconley91 <matthew.e.conley@gmail.com>
2024-10-29 20:34:01 -0400

    add buttons, add output, convert console.logs to variable declarations

--- scripts.js
+++ scripts.js
@@ -1,39 +1,37 @@
-//score tracker
 let playerScore = 0;
 let computerScore = 0;
-//loop to handle turns
-for(let i = 0; i < 5; i++){
-    //prompt:
-    const play = prompt('Open the web console to play. The game is best out of 5. Enter "rock", "paper", or "scissors" to begin!').toLowerCase();
-    //CPU player logic:
+// const rockBtn = document.querySelector('#rock');
+// const paperBtn = document.querySelector('#paper');
+// const scissorsBtn = document.querySelector('#scissors');
+const choices = document.querySelector('#choices');
+choices.addEventListener('click',(event)=>{
+    let btnPressed = event.target.id;
+    playGame(btnPressed)
+});
+
+function playGame(play){
     let randomNum = Math.floor(Math.random() * 3) + 1;
     let cpuChoice = randomNum == 1 ? 'rock' : randomNum == 2 ? 'paper' : randomNum == 3 ? 'scissors' : 'ERROR';
-    //input handling
-    if(play != 'rock' && play != 'paper' && play != 'scissors'){
-        //alerts player to invalid entry & stalls the advancement of turns
-        alert('Invalid Entry');
-        i--;
+    let result;
+    if (cpuChoice=='rock'){
+        play == 'rock' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` : 
+        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`,playerScore++) :
+        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++) : console.log('ERROR');
     }
-    else{
-        if (cpuChoice=='rock'){
-            play == 'rock' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) : 
-            play == 'paper' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`),playerScore++) :
-            play == 'scissors' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++) : console.log('massive error has occured');
-        }
-        else if (cpuChoice=='paper'){
-            play == 'rock' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++ ) : 
-            play == 'paper' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) :
-            play == 'scissors' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`), playerScore++) : console.log('massive error has occured');
-        }
-        else if (cpuChoice=='scissors'){
-            play == 'rock' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, You win!!`),playerScore++) : 
-            play == 'paper' ? (console.log(`You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`),computerScore++) :
-            play == 'scissors' ? console.log(`You picked ${play}. Computer picks ${cpuChoice}, it's a draw!`) : console.log('massive error has occured');
-        }
+    else if (cpuChoice=='paper'){
+        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++ ) : 
+        play == 'paper' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` :
+        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`, playerScore++) : console.log('ERROR');
+    }
+    else if (cpuChoice=='scissors'){
+        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`,playerScore++) : 
+        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++) :
+        play == 'scissors' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` : console.log('ERROR');
     };
-};
 //final output
-console.log(`Final Score= Player: ${playerScore} Computer: ${computerScore}`);
-playerScore > computerScore ? console.log('You won the game!') : 
-playerScore == computerScore ? console.log("It's a tie!") : 
-console.log('Computer wins the game!');
+// console.log(`Final Score= Player: ${playerScore} Computer: ${computerScore}`);
+// playerScore > computerScore ? console.log('You won the game!') : 
+// playerScore == computerScore ? console.log("It's a tie!") : 
+// console.log('Computer wins the game!');
+};
+    
