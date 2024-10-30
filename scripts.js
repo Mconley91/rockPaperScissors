5298eb3143fb413091674780101e6926f123d5d3
mconley91 <matthew.e.conley@gmail.com>
2024-10-29 20:54:36 -0400

    add DOM methods for li, add reset button w/o functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-// const rockBtn = document.querySelector('#rock');
-// const paperBtn = document.querySelector('#paper');
-// const scissorsBtn = document.querySelector('#scissors');
+const playerScoreReadout = document.querySelector('#playerScore');
+const computerScoreReadout = document.querySelector('#computerScore');
 const choices = document.querySelector('#choices');
+const output = document.querySelector('#output');
+
 choices.addEventListener('click',(event)=>{
     let btnPressed = event.target.id;
     playGame(btnPressed)
@@ -15,23 +16,39 @@ function playGame(play){
     let result;
     if (cpuChoice=='rock'){
         play == 'rock' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` : 
-        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`,playerScore++) :
-        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++) : console.log('ERROR');
+        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win the round!!`,playerScore++) :
+        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins the round!`,computerScore++) : console.log('ERROR');
     }
     else if (cpuChoice=='paper'){
-        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++ ) : 
+        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins the round!`,computerScore++ ) : 
         play == 'paper' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` :
-        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`, playerScore++) : console.log('ERROR');
+        play == 'scissors' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win the round!!`, playerScore++) : console.log('ERROR');
     }
     else if (cpuChoice=='scissors'){
-        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win!!`,playerScore++) : 
-        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins!`,computerScore++) :
+        play == 'rock' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, You win the round!!`,playerScore++) : 
+        play == 'paper' ? (result = `You picked ${play}. Computer picks ${cpuChoice}, Computer wins the round!`,computerScore++) :
         play == 'scissors' ? result = `You picked ${play}. Computer picks ${cpuChoice}, it's a draw!` : console.log('ERROR');
     };
+    let roundScore = document.createElement("li");
+    roundScore.innerText = result;
+    output.appendChild(roundScore);
+    playerScoreReadout.innerText = playerScore;
+    computerScoreReadout.innerText = computerScore;
 //final output
-// console.log(`Final Score= Player: ${playerScore} Computer: ${computerScore}`);
-// playerScore > computerScore ? console.log('You won the game!') : 
-// playerScore == computerScore ? console.log("It's a tie!") : 
-// console.log('Computer wins the game!');
+if (playerScore === 5 && computerScore === 5){
+    let victory = document.createElement('li');
+    victory.innerText = "It's a tie!";
+    output.appendChild(victory);
+}
+else if (playerScore === 5){
+    let victory = document.createElement('li');
+    victory.innerText = 'You won the game!';
+    output.appendChild(victory);
+}
+else if(computerScore === 5){
+    let victory = document.createElement('li');
+    victory.innerText = 'Computer wins the game!';
+    output.appendChild(victory);
+}
 };
     
